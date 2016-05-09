d67c9f4034afec721df7f2cf9691f84b0a93ab53
moiilopez <Moises Lopez@192.168.56.1>
2016-05-09 10:41:15 -0300

    Primeros pasos

---
+++ Controller/UsuarioController.php
@@ -0,0 +1,5 @@
+<?php
+
+class UsuarioController extends UsuarioModel {
+    
+}
---
+++ Model/Dao.php
@@ -0,0 +1,128 @@
+<?php
+
+class Dao {
+
+    protected $connection;
+    private $host = "localhost";
+    private $dbname = "pan";
+    private $usuario = "root";
+    private $senha = "mysql";
+
+    public function simpleQuery($stmt, $parameters) {
+
+        try {
+
+            $this->connection = new PDO(
+                    "mysql:host={$this->host};dbname={$this->dbname};", $this->usuario, $this->senha);
+
+            $this->connection->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
+
+            $this->connection->beginTransaction();
+            $comand = $this->connection->prepare($stmt);
+
+            if ($parameters != null) {
+                foreach ($parameters as $key => $value) {
+                    $comand->bindValue($key, $value);
+                }
+//               var_dump($parameters);
+//               var_dump($stmt);
+            }
+            $comand->execute();
+            $this->connection->commit();
+            return true;
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+            if ($this->connection != null) {
+                if ($this->connection->inTransaction()) {
+                    $this->connection->rollBack();
+                }
+            }
+            return false;
+        } finally {
+            if ($this->connection != null) {
+                $this->connection = null;
+            }
+        }
+    }
+
+    public function resultQuery($stmt, $parameters) {
+
+        try {
+
+            $this->connection = new PDO(
+                    "mysql:host={$this->host};dbname={$this->dbname};", $this->usuario, $this->senha);
+
+            $this->connection->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
+
+            $this->connection->beginTransaction();
+            $command = $this->connection->prepare($stmt);
+
+            if ($parameters != null) {
+                foreach ($parameters as $key => $value) {
+                    $listaRetorno = array();
+                    $command->bindValue($key, $value);
+                }
+            }
+//            var_dump($parameters);
+//            var_dump($stmt);
+            $command->execute();
+            $listaRetorno = array();
+            for ($i = 0; $row = $command->fetch(PDO::FETCH_ASSOC); $i++) {
+                array_push($listaRetorno, $row);
+            };
+//            var_dump($listaRetorno);
+            return $listaRetorno;
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+            if ($this->connection != null) {
+                if ($this->connection->inTransaction()) {
+                    $this->connection->rollBack();
+                }
+            }
+            return array();
+        } finally {
+            //UTILIZADO NA VERSAO DO PHP 5.6 NAS PREVIAS NAO É COMPATÍVEL
+            if ($this->connection != null) {
+                //FECHA A CONEXAO COM O BANCO DE DADOS
+                $this->connection = null;
+            }
+        }
+    }
+
+    public function multipleQuery($stmt, $parameters) {
+
+        try {
+
+            $this->connection = new PDO(
+                    "mysql:host={$this->host};dbname={$this->dbname};", $this->usuario, $this->senha);
+
+            $this->connection;
+
+            $this->connection->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
+
+            $this->connection->beginTransaction();
+
+            include 'DaoMultiple.php';
+
+            $objMultiple = new DaoMultiple();
+            $objMultiple->transMultiple($stmt, $parameters, $this->connection);
+
+            $this->connection->commit();
+
+            return true;
+        } catch (PDOException $ex) {
+            echo $ex->getMessage();
+            if ($this->connection != null) {
+                if ($this->connection->inTransaction()) {
+                    $this->connection->rollBack();
+                }
+            }
+            return false;
+        } finally {
+            if ($this->connection != null) {
+                $this->connection = null;
+            }
+        }
+    }
+
+}
